e1fc3afcbd88c76df9f2a317c2f3ef314259907b
Carlos <carlosg42@users.noreply.github.com>
2015-03-20 19:18:45 -0300

    worker: add get_task method

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -1,7 +1,7 @@
 import time
 import unittest
 
-from mock import call, Mock, patch
+from mock import call, Mock, patch, ANY
 
 from hived.worker import BaseWorker
 
@@ -48,6 +48,46 @@ class BaseWorkerTest(unittest.TestCase):
             w.run()
             sleep_times = [c[0][0] for c in sleep.call_args_list]
             self.assertLess(sleep_times[0], sleep_times[1])
+
+    def test_get_task_instantiates_task_class(self):
+        class W(BaseWorker):
+            task_class = Mock()
+
+        worker = W(Mock(), 'myqueue')
+        message = Mock()
+        task = worker.get_task(message)
+        self.assertEqual(task, W.task_class.return_value)
+        self.assertEqual(W.task_class.call_args_list, [call(message)])
+
+    def test_protected_run_sends_message_to_garbage_if_validate_message_fails(self):
+        class W(BaseWorker):
+            def validate_message(self, message):
+                self.stopped = True
+                assert False
+
+        worker = W(Mock(), 'myqueue')
+        worker.queue = Mock()
+        message, ack = Mock(), Mock()
+        worker.queue.get.return_value = (message, ack)
+
+        with patch.object(worker, 'send_message_to_garbage') as send_to_garbage_mock:
+            worker.protected_run()
+            self.assertEqual(send_to_garbage_mock.call_args_list, [call(message, ack, ANY)])
+
+    def test_protected_run_sends_message_to_garbage_if_get_task_fails(self):
+        class W(BaseWorker):
+            def get_task(self, message):
+                self.stopped = True
+                assert False
+
+        worker = W(Mock(), 'myqueue')
+        worker.queue = Mock()
+        message, ack = Mock(), Mock()
+        worker.queue.get.return_value = (message, ack)
+
+        with patch.object(worker, 'send_message_to_garbage') as send_to_garbage_mock:
+            worker.protected_run()
+            self.assertEqual(send_to_garbage_mock.call_args_list, [call(message, ack, ANY)])
 
 
 if __name__ == '__main__':
