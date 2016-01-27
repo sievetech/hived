8828c966eaa1d59907e3422022e064bae649a72a
Rodrigo Cacilhας <rodrigo.cacilhas@sieve.com.br>
2016-01-27 18:41:58 -0200

    suggestion of exception logging with no need to duplicate the code

--- hived/log.py
+++ hived/log.py
@@ -1,5 +1,5 @@
 import logging
-
+from traceback import format_exception
 import simplejson as json
 
 from hived import trail
@@ -26,5 +26,16 @@ class JsonFormatter(logging.Formatter):
         record_dict['_name'] = record.name
         if trail.get_id():
             record_dict['_trail_id'] = trail.get_id()
+
+        if record.exc_info:
+            etype, exc, tb = record.exc_info
+            record_dict['error'] = {
+                'etype': etype.__name__,
+                'exc': exc,
+                'locals': {k: repr(v)
+                           for k, v in tb.tb_frame.f_locals.iteritems()
+                           if not k.startswith('__')},
+                'traceback': format_exception(etype, exc, tb),
+            }
 
         return json.dumps(record_dict, default=json_handler)
